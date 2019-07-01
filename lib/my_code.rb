f782233076bff119ad83a8f8a802e97742f51501
David Little <littledavid175@gmail.com>
2019-07-01 17:20:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,7 +21,11 @@ end
 
 def map_to_double(source_array)
   double = []
-  
+  i = 0 
+  while i < source_array.length do
+    no.push(source_array[i])
+    i += 1
+  end
 end
 
 def reduce_to_total(source_array, starting_point = 0)
