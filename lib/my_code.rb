ba7d7e834afdc999463dfd546ea72aeaefe24b31
David Little <littledavid175@gmail.com>
2019-07-01 17:40:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,7 +60,14 @@ i = 0
 end
 
 def reduce_to_any_true(source_array)
-
+i = 0
+  while i < source_array.length do
+    if source_array[i] === false
+      return false
+    end
+    i += 1
+  end
+end
 
 
 
